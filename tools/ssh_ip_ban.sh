f9ebd3a451b63de247d675a14bb282d6ef05bd95
Sam Taylor <sam@taylornetwork.ca>
2022-01-06 17:10:24 -0500

    Updated ban script

--- tools/ssh_ip_ban.sh
+++ tools/ssh_ip_ban.sh
@@ -1,12 +1,47 @@
 #!/bin/bash
-if [ -f './banlist' ]
+
+echo Starting...
+
+if [ "$(sudo ufw status | grep -i inactive)" ]
 then
-	mv banlist banlist.backup
+	echo UFW is inactive, exiting...
+	exit
 fi
 
-sudo cat /var/log/auth.log | grep -v CRON | grep from | grep sshd | grep 'preauth' | grep -E -o "([0-9]{1,3}[\.]){3}[0-9]{1,3}" | sort -u > banlist
+if ! [ -f './whitelist' ]
+then
+	echo Whitelist not found, created at $(pwd)/whitelist
+	touch whitelist
+fi
 
-for ip in $(cat banlist)
+sudo cat /var/log/auth.log | grep preauth | grep -E -o "([0-9]{1,3}[\.]){3}[0-9]{1,3}" | sort -u > temp_blacklist
+
+echo Found $(cat temp_blacklist | wc -l) possible offending IP addresses...
+
+if [ -f './blacklist' ]
+then
+	echo Existing blacklist file found.
+	if ! [ "$(cat temp_blacklist | wc -l)" -eq "$(cat blacklist | wc -l)" ]
+	then
+		echo ** Different number of blacklist items **
+		wc -l blacklist temp_blacklist
+
+		mv blacklist blacklist.backup
+
+		echo blacklist => blacklist.backup
+	fi
+fi
+
+echo Removing whitelist IPs from blacklist file...
+comm -23 <(sort temp_blacklist) <(sort whitelist) > blacklist
+
+mv temp_blacklist blacklist
+echo temp_blacklist => blacklist
+
+echo Starting to ban all blacklist in 5 seconds...
+sleep 5
+
+for ip in $(cat blacklist)
 do
 	if ! [ "$(sudo ufw status numbered | grep $ip)" ]
 	then
